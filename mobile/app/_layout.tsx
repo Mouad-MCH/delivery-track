fc19fc42028cb133088e8c6692c29518382b481a
salima <sahisalima77@gmail.com>
2026-08-31 13:16:27 +0200

    configuration stack navigation

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,28 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Livraisons",
+        }}
+      />
+
+      <Stack.Screen
+        name="delivery/[id]"
+        options={{
+          title: "Détail de la livraison",
+        }}
+      />
+
+      <Stack.Screen
+        name="delivery/new"
+        options={{
+          title: "Nouvelle livraison",
+        }}
+      />
+    </Stack>
+  );
 }
