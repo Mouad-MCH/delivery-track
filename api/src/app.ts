20a5d31e5837533d6606ef69be5b6b23d0b09261
Mehdi El-Hajjame <el-hajjame@outlook.com>
2026-09-02 12:19:57 +0100

    feat(api): add delivery routes and error handling middleware

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,4 +1,8 @@
 import express from "express";
+import { deliveryRouter } from "./routes/route";
+import { errorHandler } from "./middleware/error.middleware";
 export const app = express();
 
 app.use(express.json());
+app.use("/api/deliveries", deliveryRouter);
+app.use(errorHandler);
